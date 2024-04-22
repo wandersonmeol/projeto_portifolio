40d2e06f23cac390a897e2a8334b7b92dff17434
Wanderson Mendes Oliveira <147285083+wandersonmeol@users.noreply.github.com>
2024-04-22 07:12:40 -0300

    Coloquei o botão (mostrar mais) para funcionar

--- SRC/js/index.js
+++ SRC/js/index.js
@@ -14,10 +14,23 @@
 
 // console.log(document.querySelector('.btn-mostrar-projetos')); -> Só para exemplificar.
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
-const projetosInativos = 
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
 
 botaoMostrarProjetos.addEventListener('click', () => {
-    console.log('teste');
     // Passo 3 - adicionar a classe "ativo" nos projetos escondidos.
-
+    mostrarMaisProjetos(); // Refatoração.
+    
+    // Objetivo 2 - esconder o botão de mostrar mais.
+    // Passo 1 - pegar o botão e esconder ele.
+    esconderBotao(); // // Refatoração.
 })
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
