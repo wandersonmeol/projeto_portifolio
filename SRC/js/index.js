cc8bbcde4e725519b270151791330d56f1a7de2d
Wanderson Mendes Oliveira <147285083+wandersonmeol@users.noreply.github.com>
2024-04-21 21:31:58 -0300

    Configurando JS no botão Mostrar Mais.

--- SRC/js/index.js
+++ SRC/js/index.js
@@ -1,3 +1,23 @@
 /* 
-    Objetivo 1 - Quando o usuário clicar no botão
+    Objetivo 1 - Quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html.
+        Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário cliecar em cima dele.
+        Passo 2 - identificar o clique no botão.
+        Passo 3 - adicionar a classe "ativo" nos projetos escondidos.
+
+    Objetivo 2 - esconder o botão de mostrar mais
+        Passo 1 - pegar o botão e esconder ele
 */
+
+// Objetivo 1 - Quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html.
+
+// Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário cliecar em cima dele.
+
+// console.log(document.querySelector('.btn-mostrar-projetos')); -> Só para exemplificar.
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = 
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    console.log('teste');
+    // Passo 3 - adicionar a classe "ativo" nos projetos escondidos.
+
+})
